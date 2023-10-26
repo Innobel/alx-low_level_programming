0905cf8be96dea580fe0616766e7249d4708a759
innobel <innocentabel11@gmail.com>
2023-10-26 16:39:25 -0700

    a function that returns 1 if the input integer is a prime number, otherwise return 0.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,25 +2,43 @@
 #include <string.h>
 
 /**
+* is_palindrome_recursive - Helper function to check if a string is a palindrom
+* @s: The string to be checked.
+* @start: The starting index of the current comparison.
+* @end: The ending index of the current comparison.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
+int is_palindrome_recursive(char *s, int start, int end)
+{
+if (start >= end)
+{
+return (1); /* Base case: It's a palindrome. */
+}
+
+if (s[start] != s[end])
+{
+return (0); /* Characters do not match, not a palindrome. */
+}
+
+return (is_palindrome_recursive(s, (start + 1), (end - 1)));
+}
+
+/**
 * is_palindrome - Checks if a string is a palindrome.
 * @s: The string to be checked.
 *
-* Return: 1 if s is a palindrome, 0 otherwise.
+* Return: 1 if the string is a palindrome, 0 otherwise.
 */
 int is_palindrome(char *s)
 {
-int i = 0;
-int j = strlen(s) - 1;
+int length = strlen(s);
 
-while (i < j)
+if (length <= 1)
 {
-if (s[i] != s[j])
-{
-return (0); /* Not a palindrome. */
-}
-i++;
-j--;
+return (1); /* Empty string or single-character string is a palindrome. */
 }
 
-return (1); /* It's a palindrome. */
+return (is_palindrome_recursive(s, 0, (length - 1)));
 }
+
