0324987cff6a4dac785eef5a96bbf90af922b252
innobel <innocentabel11@gmail.com>
2023-10-26 15:29:23 -0700

    a function that returns the value of x raised to the power of y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,15 +12,16 @@ int _pow_recursion(int x, int y)
 if (y < 0)
 {
 /* Exponent is negative, so return -1 as an error. */
-return -1;
+return (-1);
 }
 else if (y == 0)
 {
 /* Base case: Any number raised to the power of 0 is 1. */
-return 1;
-} else
+return (1);
+}
+else
 {
 /* Recursively calculate x^y by multiplying x with x^(y-1). */
-return x * _pow_recursion(x, y - 1);
+return (x * _pow_recursion(x, y - 1));
 }
 }
