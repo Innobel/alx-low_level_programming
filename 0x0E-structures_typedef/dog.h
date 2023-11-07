d70aa7d9de88a5e61f4ca6df9ae6f56e0f321a6e
innobel <innocentabel11@gmail.com>
2023-11-06 16:04:53 -0800

    header file for structure and typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,25 @@
 #define DOG_H
 
 #define NULL ((void *)0)
-struct dog 
+
+/**
+ * struct dog - A structure defining a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
+/* Function declaration */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
+void free_dog(dog_t *d);
+
 #endif /* DOG_H */
