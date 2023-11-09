1c2f6c8e8bc2bc0c5fe8ca05829ed18ca5bcfa6b
innobel <innocentabel11@gmail.com>
2023-11-08 20:55:30 -0800

    Function pointer header file containing all the project prototype

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
-#define NULL ((void *)0)
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
