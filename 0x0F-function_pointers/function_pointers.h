2b93bc9e918ac2336298f475bf146810981ca747
innobel <innocentabel11@gmail.com>
2023-11-09 05:07:01 -0800

    function header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -7,4 +7,4 @@ void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* _HEADER_ */
+#endif /* _FUNCTION_POINTERS_H_ */
