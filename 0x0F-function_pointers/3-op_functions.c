56190aa3ddf48472336970f1eaf4e94bd833742b
innobel <innocentabel11@gmail.com>
2023-11-09 17:42:20 -0800

    contains function definitions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* add - Add two integers
+* @a: The first integer
+* @b: The second integer
+*
+* Return: The sum of a and b
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* sub - Subtract two integers
+* @a: The first integer
+* @b: The second integer
+*
+* Return: The difference of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* mul - Multiply two integers
+* @a: The first integer
+* @b: The second integer
+*
+* Return: The product of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* div - Divide two integers
+* @a: The first integer
+* @b: The second integer
+*
+* Return: The result of a divided by b
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* mod - Compute the modulo of two integers
+* @a: The first integer
+* @b: The second integer
+*
+* Return: The remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
