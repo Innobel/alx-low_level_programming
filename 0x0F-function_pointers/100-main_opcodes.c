317a075e151ffa1891497a366988c3d797b9c422
innobel <innocentabel11@gmail.com>
2023-11-09 17:54:38 -0800

    a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * print_opcodes - Print opcodes of a given memory region in hexadecimal.
+ * @main: A pointer to the memory region.
+ * @num_bytes: The number of bytes to print.
+ */
 void print_opcodes(unsigned char *main, int num_bytes);
-
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, 1 for an argument count error, \
+ * and 2 for a negative byte count.
+ */
 int main(int argc, char **argv)
 {
 if (argc != 2)
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 
 int num_bytes = atoi(argv[1]);
@@ -16,14 +26,18 @@ int num_bytes = atoi(argv[1]);
 if (num_bytes < 0)
 {
 printf("Error\n");
-return 2;
+return (2);
 }
 
 print_opcodes((unsigned char *)main, num_bytes);
 
-return 0;
+return (0);
 }
-
+/**
+ * print_opcodes - Print opcodes of a given memory region in hexadecimal.
+ * @main: A pointer to the memory region.
+ * @num_bytes: The number of bytes to print.
+ */
 void print_opcodes(unsigned char *main, int num_bytes)
 {
 int i;
