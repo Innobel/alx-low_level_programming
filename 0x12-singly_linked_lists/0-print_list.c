d304a3f291d31e22d0ff8009448df00d9e8bbcee
innobel <innocentabel11@gmail.com>
2023-11-16 06:46:40 -0800

    A function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include "stdio.h"
 
-int _strlen(char *s);
 /**
  * print_list - Prints all element in list
  * @h: pointer to linked list
@@ -11,9 +10,6 @@ size_t print_list(const list_t *h)
 {
 	const list_t *ptr;
 	size_t no_of_nodes = 0;
-
-	if (h == NULL)
-		return (0);
 
 	ptr = h;
 
@@ -25,31 +21,12 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%lu] %s\n", _strlen(ptr->str), ptr->str);
-			no_of_nodes++;
+			printf("[%u] %s\n", ptr->len,  ptr->str);
 		}
 
 		ptr = ptr->next;
+		no_of_nodes++;
 	}
 
 	return (no_of_nodes);
-}
-
-/**
- * _strlen - calculate the len of a string.
- * @s: character pointer.
- * Return: len
- */
-
-int _strlen(char *s)
-{
-	int len = 0, i = 0;
-
-	while (s[i] != '\0')
-	{
-		len++;
-		i++;
-	}
-
-	return (len);
 }
