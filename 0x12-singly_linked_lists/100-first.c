ea17cb9cdc1649889850777e1afb4c253e770ddd
innobel <innocentabel11@gmail.com>
2023-11-16 20:11:47 -0800

    A function that prints a message before main is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * before_main - Prints a specific message before main is exexuted.
+ */
+
+void befor_main(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("I bore my house upon my back!\n");
+}
