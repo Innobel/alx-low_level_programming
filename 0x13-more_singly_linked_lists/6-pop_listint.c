95baaa53a3715a039521f6119ad843c57f95917f
innobel <innocentabel11@gmail.com>
2023-11-22 05:43:18 -0800

    A function that deletes the head node of alistint_t linked list, and returns the head node's data (n).

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,22 +1,38 @@
 #include "lists.h"
+
 /**
- * pop_listint - Deletes the head node of a listint_t linked list.
- *               Returns the head node's data.
- * @head: A pointer to a pointer to the head of the linked list.
- * Return: The data of the deleted head node, or 0 if the list is empty.
- */
+* pop_listint - Deletes the head node of a listint_t linked list.
+* @head: A pointer to a pointer to the head of the linked list.
+*
+* Return: The data of the deleted head node, or 0 if the list is empty.
+*/
 int pop_listint(listint_t **head)
 {
-	listint_t *temp;
-	int data;
+listint_t *temp;
+int data;
 
-	if (head == NULL || *head == NULL)
-		return (0);
+if (head == NULL || *head == NULL)
+return (0);
 
-	temp = (*head)->next;
-	data = (*head)->n;
-	free(*head);
-	*head = temp;
+temp = *head;
+*head = (*head)->next;
 
-	return (data);
+/*
+* Check if the new head node exists.
+* If it does, retrieve the data from the new head node.
+* Otherwise, if the list is now empty, retrieve the data from the original
+* head node.
+*/
+if (*head != NULL) {
+data = (*head)->n;
+free(temp);
+temp = NULL;
+} else {
+data = temp->n;
+free(temp);
+*head = NULL;  /* Update head to NULL since the list is now empty */
 }
+
+return (data);
+}
+
