c944d0e887b78325dcd3c88dd85945b52539e34d
innobel <innocentabel11@gmail.com>
2023-11-21 20:59:23 -0800

    A function that returns the sum of all the date(n) of a listint_t linked list.

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,21 +1,26 @@
 #include "lists.h"
+/**
+ * sum_listint - Returns the sum of all the data (n) in a listint_t linked list
+ * @head: A pointer to the head of the linked list.
+ * Return: The sum of all the data, or 0 if the list is empty.
+ */
 
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *ptr;
 
 	if (head == NULL)
 		return (0);
 
-	ptr = head;
 
-	while (ptr != NULL)
+	while (head->next != NULL)
 	{
-		sum = ptr->n;
-		ptr = ptr->next;
+		sum = head->n;
+		head = head->next;
 	}
-	
+
+	sum += head->n;
+
 	return (sum);
 }
 
