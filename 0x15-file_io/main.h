24775677e5b934eea8d5ca580953d62045070188
innobel <innocentabel11@gmail.com>
2023-11-30 02:08:40 -0800

    Header file containing the project prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_elf_header_info(Elf64_Ehdr *header);
+
 #endif /* MAIN_H */
