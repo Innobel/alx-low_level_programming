387e85f34e4cbbdcf698408994fd09da8c7bc3e9
innobel <innocentabel11@gmail.com>
2023-11-30 21:10:11 -0800

    header file containing all the prototype used for the project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
