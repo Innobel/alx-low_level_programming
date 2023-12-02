6d5001fcab28355e221bbadc57f27c9384d34510
innobel <innocentabel11@gmail.com>
2023-12-01 20:51:55 -0800

    Header file containing all prototype used for the file io project

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,9 +7,13 @@
 #include <stddef.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <elf.h>
 
 #define BUFFER_SIZE 1024
+#define EN_NIDENT
 
+void display_elf_header_info(Elf64_Ehdr *elf_header);
+void error_message(const char *message);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
